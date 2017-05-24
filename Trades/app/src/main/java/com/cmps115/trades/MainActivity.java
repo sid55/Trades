fc4f5db80f919f2db788e6bc34f298de64620c9a
Hasaan Javed <hjaved@ucsc.edu>
2017-05-24 14:58:50 -0700

    set up firebase

--- Trades/app/src/main/java/com/cmps115/trades/MainActivity.java
+++ Trades/app/src/main/java/com/cmps115/trades/MainActivity.java
@@ -4,18 +4,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
-    DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mDatabaseProfRef;
 
-    public static final String tag = "On Create!"
+    public static final String tag = "On Create!";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i(tag, "onCreate");
+
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mDatabaseProfRef = mFirebaseDatabase.getReference().child("profiles");
     }
 
 }
