7816223728dcf0fe117e9e947c745f0a14d19cb6
Daniel Tjandra <daniel.tjandra96@gmail.com>
2017-06-01 09:05:21 -0700

    Got Listing push to database to work

--- Trades/app/src/main/java/com/cmps115/trades/SellPage.java
+++ Trades/app/src/main/java/com/cmps115/trades/SellPage.java
@@ -5,12 +5,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class SellPage extends AppCompatActivity {
+    //View Refs
+    private Button vSubmit;
+    private EditText vSkill;
+    private EditText vDesc;
 
+    //Database Refs
     private FirebaseDatabase mDatabase;
     private DatabaseReference mProfileRef;
     private DatabaseReference mListingRef;
@@ -27,6 +33,11 @@ public class SellPage extends AppCompatActivity {
         });
     }
 
+    private void writeNewListing(String name, String desc){
+        ListingEntry listing = new ListingEntry(name, desc);
+        mListingRef.push().setValue(listing);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,8 +45,23 @@ public class SellPage extends AppCompatActivity {
         setContentView(R.layout.activity_sell_page);
         initSubmit();
 
+        //View References
+        vSubmit = (Button) findViewById(R.id.Submit_Button);
+        vSkill = (EditText) findViewById(R.id.Skill_Text);
+        vDesc = (EditText) findViewById(R.id.Desc_Text);
+
+        //Database References
         mDatabase = FirebaseDatabase.getInstance();
         mProfileRef = mDatabase.getReference().child("profiles");
         mListingRef = mDatabase.getReference().child("listings");
+
+        vSubmit.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                String lName = vSkill.getText().toString();
+                String lDesc = vDesc.getText().toString();
+                writeNewListing(lName, lDesc);
+            }
+        });
     }
 }
