b8b410b6fd93de1922160fd2b4fa19bc8d92b564
bcwan <bcwan8@gmail.com>
2017-06-01 18:53:27 -0700

    Branching Brian

--- Trades/app/src/main/java/com/cmps115/trades/MainActivity.java
+++ Trades/app/src/main/java/com/cmps115/trades/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
