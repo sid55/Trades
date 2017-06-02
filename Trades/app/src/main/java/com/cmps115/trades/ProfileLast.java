81cc2b9802937df8cc212981545d9f6d8059cc66
sid55 <sidharthgilela@gmail.com>
2017-06-01 18:26:38 -0700

    Location API Works!

--- Trades/app/src/main/java/com/cmps115/trades/ProfileLast.java
+++ Trades/app/src/main/java/com/cmps115/trades/ProfileLast.java
@@ -3,17 +3,29 @@ package com.cmps115.trades;
 import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.jar.*;
 
+import android.*;
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.graphics.Bitmap;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -27,6 +39,11 @@ public class ProfileLast extends AppCompatActivity {
     private Button button;
     private Button saveProf;
     private Uri imageUri;
+
+    //for getting location
+    private TextView textView;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
 
     //String values
     private String editFirstName;
@@ -49,6 +66,43 @@ public class ProfileLast extends AppCompatActivity {
         imageView = (ImageView) findViewById(R.id.image);
         button = (Button) findViewById(R.id.imageSubmit);
         saveProf = (Button) findViewById(R.id.submit);
+
+        //For location API
+        textView = (TextView) findViewById(R.id.textView2);
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                textView.append("\n" + location.getLatitude() + " " + location.getLongitude());
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        };
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{
+                        Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.INTERNET
+                }, 10);
+                return;
+            }
+        }else{
+            configureButton();
+        }
 
         //Database References
         mDatabase = FirebaseDatabase.getInstance();
@@ -85,6 +139,25 @@ public class ProfileLast extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 openGallery();
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 10:
+                if (grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    configureButton();
+                return;
+        }
+    }
+
+    public void configureButton(){
+        saveProf.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                locationManager.requestLocationUpdates("gps", 5000, 0, locationListener);
             }
         });
     }
