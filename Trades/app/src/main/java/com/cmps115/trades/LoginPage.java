7ac51c812236e94581cf485d6a49a67374105fa6
bcwan <bcwan8@gmail.com>
2017-06-01 22:52:09 -0700

    Added packages

--- Trades/app/src/main/java/com/cmps115/trades/LoginPage.java
+++ Trades/app/src/main/java/com/cmps115/trades/LoginPage.java
@@ -1,8 +1,32 @@
 package com.cmps115.trades;
 
-/**
- * Created by bcwan on 4/26/17.
- */
+import android.app.Application;
+import android.content.Context;
+import android.content.Intent;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInApi;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+
+import java.util.jar.Attributes;
+
 
 public class LoginPage {
 
