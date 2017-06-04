f227d8dc64492d8fbb0fb5b4cd0a242661002e6c
bcwan <bcwan8@gmail.com>
2017-06-04 15:51:37 -0700

    Added the signin button

--- Trades/app/src/main/java/com/cmps115/trades/FragmentThree.java
+++ Trades/app/src/main/java/com/cmps115/trades/FragmentThree.java
@@ -11,9 +11,41 @@ import android.widget.Button;
 import android.widget.ProgressBar;
 
 
-public class FragmentThree extends Fragment {
+import android.app.Application;
+import android.content.Context;
+import android.content.Intent;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInApi;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+
+import java.util.jar.Attributes;
+
+
+
+public class FragmentThree extends Fragment, AppCompatActivity implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener{
 
     private Button nextBut;
+
+    SignInButton signInBtn;
+
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_three, container, false);
--- Trades/app/src/main/java/com/cmps115/trades/Login.java
+++ Trades/app/src/main/java/com/cmps115/trades/Login.java
@@ -5,8 +5,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
-import android.support.multidex.MultiDex;
-import android.support.multidex.MultiDexApplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
