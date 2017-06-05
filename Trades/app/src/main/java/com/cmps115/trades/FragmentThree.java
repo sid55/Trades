1ed881cbeb591be3a64eba13008cca6597f3ea71
bcwan <bcwan8@gmail.com>
2017-06-04 17:14:26 -0700

    After trying to combine Google Login API to fragmentation, consenus to link button onto a separate page

--- Trades/app/src/main/java/com/cmps115/trades/FragmentThree.java
+++ Trades/app/src/main/java/com/cmps115/trades/FragmentThree.java
@@ -11,41 +11,9 @@ import android.widget.Button;
 import android.widget.ProgressBar;
 
 
-import android.app.Application;
-import android.content.Context;
-import android.content.Intent;
-import android.provider.ContactsContract;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-
-
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInApi;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.common.api.Status;
-
-import java.util.jar.Attributes;
-
-
-
-public class FragmentThree extends Fragment, AppCompatActivity implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener{
+public class FragmentThree extends Fragment {
 
     private Button nextBut;
-
-    SignInButton signInBtn;
-
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_three, container, false);
