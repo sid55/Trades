f3fadec1bba52f60c4b883764de9aebf59c12cbc
sid55 <sidharthgilela@gmail.com>
2017-06-06 22:28:02 -0700

    Changed code by adding image decoding into MyProfile.java file

--- Trades/app/src/main/java/com/cmps115/trades/MyProfile.java
+++ Trades/app/src/main/java/com/cmps115/trades/MyProfile.java
@@ -1,8 +1,11 @@
 package com.cmps115.trades;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -40,6 +43,12 @@ public class MyProfile extends AppCompatActivity {
         name = (TextView) findViewById(R.id.first);
         email = (TextView) findViewById(R.id.email);
         phone = (TextView) findViewById(R.id.phonenumber);
+
+        //decode encoded image from server and paste into imageView
+        String encodedString = ProfileLast.currUser.getMyImage();
+        byte[] decodedString = Base64.decode(encodedString, Base64.DEFAULT);
+        Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+        imageView.setImageBitmap(decodedByte);
 
         name.setText(ProfileLast.currUser.getName());
         email.setText(ProfileLast.currUser.getEmail()+".edu");
